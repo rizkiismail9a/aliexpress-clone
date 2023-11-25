33e1777f04d96401dc998d95dd03f41aa2014864
rizkiismail <laksmimentari@gmail.com>
2023-11-25 21:03:50 +0700

    fixing deployment bug

--- pages/auth.vue
+++ pages/auth.vue
@@ -2,7 +2,7 @@
   <div id="AuthPage" class="w-full h-[100vh] bg-white">
     <div class="w-full flex items-center justify-center p-5 border-b border-b-gray-300">
       <NuxtLink to="/" class="min-w-[170px]">
-        <img src="/images/Aliexpress-logo.png" width="170" />
+        <img src="/images/AliExpress-logo.png" width="170" />
       </NuxtLink>
     </div>
     <div class="max-w-[400px] mx-auto px-2">
